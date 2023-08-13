78df3503729a78e09924b949051f341de6dba70e
IEatSushi <2klaus.4peter@gmail.com>
2023-08-13 22:31:52 +0200

    make matplotlib slower (no loading time, no 20GB RAM usage)

--- timeline_viewer_plt.py
+++ timeline_viewer_plt.py
@@ -11,7 +11,7 @@ class TimelineViewer:
 
 	def __init__(self, image_folder):
 		self.image_folder = image_folder
-		self.image_arrays = []
+		self.image_files = []
 		self.current_index = 0
 		self.canvas = None
 
@@ -20,17 +20,16 @@ class TimelineViewer:
 
 	def _preload_images(self):
 		# Get a list of image file names in the folder
-		image_files = [os.path.join(self.image_folder, f) for f in os.listdir(self.image_folder) if f.lower().endswith(('.png'))]
-		self.image_arrays = [np.asarray(Image.open(f).convert("RGBA")) for f in image_files]
+		self.image_files = [os.path.join(self.image_folder, f) for f in os.listdir(self.image_folder) if f.lower().endswith(('.png'))]
 		print("loaded images")
 	
 	# Create a function to update the displayed image
 	def update_image(self, val):
 		self.current_index = int(val)
-		# set canvas' internal image data bypassing some expensive checks
-		self.canvas._A = self.image_arrays[self.current_index]
-		self.canvas._imcache = self.image_arrays[self.current_index]
-		#self.canvas.set_data(self.image_arrays[self.current_index])
+		img_array = np.asarray(Image.open(self.image_files[self.current_index]).convert("RGBA"))
+		#set canvas' internal image data bypassing some expensive checks
+		self.canvas._A = img_array
+		self.canvas._imcache = img_array
 
 	def _show(self):
 		fig, ax = plt.subplots()
@@ -38,14 +37,14 @@ class TimelineViewer:
 		plt.tight_layout()
 		
 		# display the initial image
-		self.canvas = ax.imshow(self.image_arrays[self.current_index], interpolation="none")
+		img_array = np.asarray(Image.open(self.image_files[self.current_index]).convert("RGBA"))
+		self.canvas = ax.imshow(img_array, interpolation="none")
 
 		# Create a slider
-		num_images = len(self.image_arrays)
+		num_images = len(self.image_files)
 		ax_slider = plt.axes([0.2, 0.02, 0.65, 0.03], facecolor='lightgoldenrodyellow')
 		slider = Slider(ax_slider, 'Image', 0, num_images - 1, valinit=self.current_index, valstep=1)
 		slider.on_changed(self.update_image)
-		self.canvas.set_data(self.image_arrays[0])
 
 		# Display the plot
 		plt.show()
