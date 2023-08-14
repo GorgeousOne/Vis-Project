09b7d52fb2edba82fb54c75a9f50373405ffd32e
IEatSushi <2klaus.4peter@gmail.com>
2023-08-14 04:49:21 +0200

    add grid, tick labels, axes labels

--- color_frequencies_graph.py
+++ color_frequencies_graph.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from time_utils import *
-from matplotlib.ticker import FuncFormatter
 import matplotlib.ticker as ticker
+
+from time_utils import *
+
 
 data = pd.read_csv('data/color_frequencies.csv', index_col="ms")
 df = pd.DataFrame(data)
@@ -11,28 +12,38 @@ df = pd.DataFrame(data)
 area_colors = tuple(data.columns)
 # creates stacked area plot with custom colors, no legend and 0 line width
 ax = df.plot.area(title="Use of colors over time", color=area_colors, legend=False, lw=0)
+plt.tight_layout()
 
-# set ticks at every datafrane x value and make label vertical
-plt.xticks(df.index, rotation=90)
-plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(1e6))
-plt.gca().yaxis.set_minor_locator(ticker.MultipleLocator(1e5))
+# draw y lines below graph
+ax.grid(axis='y')
+ax.set_axisbelow(True)
+
 
 def format_large_number(y, pos):
     return f'{y:,.0f}'
 
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_large_number))
+def format_time(y, pos):
+    return datetime.fromtimestamp(y * 0.001 + PLACE_START).strftime("%d.%#m.  %#I%p")
 
-plt.tight_layout()
 
 # convert x ticks from ms to datetime
-formatter = FuncFormatter(timestamp_to_str)
-ax.xaxis.set_major_formatter(formatter)
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_large_number))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1e6 / 2))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(1e5))
+# set ticks at every datafrane x value and make label vertical
+plt.xticks(df.index, rotation=90)
+
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_time))
+ax.xaxis.set_major_locator(ticker.MultipleLocator(time_to_ms(6)))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(time_to_ms(1)))
+
+plt.xlabel('Time')
+plt.ylabel('Placed pixels')
 
 # highlight every nth tick (maybe highlight new days?)
-n = 24
-for i, label in enumerate(ax.get_xticklabels()):
-    if (i + 1) % n == 0:  # +1 to account for 0-based indexing
-        label.set_fontweight('bold')
+for label in ax.get_xticklabels()[3::4]:
+    label.set_fontweight('bold')
+
 
 # add black hatch to white, set line width back to 1
 white_area = ax.collections[-1]
